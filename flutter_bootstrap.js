069dead31ff97c23a3cbea64a23c92dc7f45caba
Grupo Becm <direccion@grupobecm.com>
2026-03-03 19:26:49 -0600

    deploy: add complete SPEI participants catalog

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3714388661" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "320175066" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
