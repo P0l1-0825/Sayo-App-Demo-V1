6a53754e7f207a000958e7b9d3e553ad3d9f1d97
Grupo Becm <direccion@grupobecm.com>
2026-03-03 23:42:49 -0600

    deploy: rebuild from verified main source

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1075333092" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4174252061" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
